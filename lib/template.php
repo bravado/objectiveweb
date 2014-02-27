5f27c8aacc97ec94ad5c619c306010e7102f32e4
Guilherme Barile <guigouz@gmail.com>
2014-02-27 15:37:48 -0300

    new template functions

--- lib/template.php
+++ lib/template.php
@@ -16,3 +16,28 @@ defined('SMARTY_DIR') || define('SMARTY_DIR', dirname(__FILE__).'/smarty/');
 if(SMARTY_DIR) {
     require_once(SMARTY_DIR . 'Smarty.class.php');
 }
+else {
+    return;
+}
+
+function assign($var, $value) {
+    global $smarty;
+
+    $smarty->assign($var, $value);
+}
+
+function aassign($values) {
+    global $smarty;
+
+    foreach($values as $key => $value) {
+        $smarty->assign($key, $value);
+    }
+}
+
+function render($template, $context = null) {
+    global $smarty;
+
+    $smarty->display("$template.tpl", $context);
+
+    exit();
+}
